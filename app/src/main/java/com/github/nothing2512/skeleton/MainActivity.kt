829d18688802fc7c449609e32d6ac1b1d41f5ef2
nothing2512 <blank345red@gmail.com>
2021-12-29 01:50:06 +0700

    first commit

---
+++ app/src/main/java/com/github/nothing2512/skeleton/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.nothing2512.skeleton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
